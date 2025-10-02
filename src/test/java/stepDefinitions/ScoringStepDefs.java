b55332c12e5630a4460560926b075ad52b178139
Felix Almqvist <kiwibirdi@hotmail.com>
2025-10-02 16:55:41 +0200

    Updated the pom file to have access to -Cucumber -Selenium -JUnit Added folders/init files for automated testing with a feature and a stepdef file

--- src/test/java/stepDefinitions/ScoringStepDefs.java
+++ src/test/java/stepDefinitions/ScoringStepDefs.java
@@ -1,6 +1,7 @@
 package stepDefinitions;
 
 import io.cucumber.java.After;
+import io.cucumber.java.Before;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -11,21 +12,25 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 public class ScoringStepDefs {
 
     WebDriver driver;
+    boolean decathlon = true;
 
     @After
-    public void tearDown(){
+    public void tearDown() {
         assert driver != null;
-        driver.quit();
+        //driver.quit();
     }
 
     @Given("That i am on the correct page on {string}")
-    public void startWebsite(String browser){
+    public void startWebsite(String browser) {
         switch (browser.toLowerCase()) {
             case "chrome":
                 driver = new ChromeDriver();
@@ -41,7 +46,87 @@ public class ScoringStepDefs {
                 fail();
         }
         driver.get("http://localhost:8080/");
-        //hitta en assert för att dubbelkolla att hemsidan är uppe
+        assertEquals("Name", driver.findElement(By.cssSelector("label[for='name']")).getText());
+        //Tycker inte om att kolla på name fältet men ingen ID eller speciel class för title
     }
 
+    @When("I fill in {string}")
+    public void fillInName(String name) {
+        //eventually add a check for if already in standings
+        driver.findElement(By.cssSelector("#name")).sendKeys(name);
+        driver.findElement(By.cssSelector("#add")).click();
+        driver.findElement(By.cssSelector("#name2")).sendKeys(name);
+    }
+
+    @And("I pick {string}")
+    public void fillInSport(String sport) {
+        WebElement tmp = driver.findElement(By.cssSelector("#event"));
+        Select dropdown = new Select(tmp);;
+        if (decathlon) {
+            switch (sport.toLowerCase()) {
+                case "100m":
+                    dropdown.selectByValue("100m");
+                    break;
+                case "110mhurdles":
+                    break;
+                case "400m":
+                    dropdown.selectByValue("400m");
+                    break;
+                case "1500m":
+                    break;
+                case "discusthrow":
+                    break;
+                case "highjump":
+                    break;
+                case "javelinthrow":
+                    break;
+                case "longjump":
+                    dropdown.selectByValue("longJump");
+                    break;
+                case "polevault":
+                    break;
+                case "shotput":
+                    dropdown.selectByValue("shotPut");
+                    break;
+                default:
+                    System.out.println("No sport under: " + sport + " in Decathlon");
+            }
+
+        } else { //no way to choose here yet revist once availible
+            switch (sport) {
+                case "100m":
+                    break;
+
+            }
+
+        }
+
+    }
+
+    @And("I put in my {string}")
+    public void PutInResult(String result) {
+        driver.findElement(By.cssSelector("#raw")).sendKeys(result);
+
+    }
+
+    @Then("I submit and verify {string}")
+    public void SubmitAndVerify(String totalScore) {
+        driver.findElement(By.cssSelector("#save")).click();
+
+        assertEquals(totalScore, findTotalscoreOfCompetitor(driver.findElement(By.cssSelector("#name2")).getText()));
+    }
+
+    public String findTotalscoreOfCompetitor(String name){
+        List<WebElement> standings = driver.findElements(By.cssSelector("#standings tr"));
+        for(WebElement cell : standings)
+        {
+            if(cell.getText().contains(name))
+            {
+
+                List<WebElement> cells = driver.findElements(By.tagName("td"));
+                return cells.getLast().getText();
+            }
+        }
+        return null;
+    }
 }
--- src/test/resources/features/scoring.feature
+++ src/test/resources/features/scoring.feature
@@ -1,13 +1,24 @@
 Feature: Check for correct calculation for scores
 
+  Scenario:
+    Given That i am on the correct page on "chrome"
+    When  I fill in "Anna"
+    And   I pick "LongJump"
+    And   I put in my "300"
+    Then  I submit and verify "66"
+
   Scenario Outline: checking the score calculations Positive tests
     Given That i am on the correct page on "<Browser>"
     When  I fill in "<Name>"
     #And  I pick which "<Category>"  hepa vs deca kom inte på bättre namn
     And   I pick "<Sport>"
-    And   I put in my "<Score>"
-    Then  I submit and verify
+    And   I put in my "<Result>"
+    Then  I submit and verify "<TotalScore>"
 
     Examples:
+      | Browser | Name | Sport | Result | TotalScore |
+      |         | anna | 100m  | 300    | 66         |
+
+
 
 
